ff9766a29f857b40e1d4f80ef6634ed2c923a4ef
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2024-01-09 15:17:50 +0900

    Update missumi_driver_test.ino

--- missumi_driver_test.ino
+++ missumi_driver_test.ino
@@ -1,1 +1,36 @@
 
+//24V driver with PUL, DIR, ENA and ALM terminals
+//Driver: 7& 8 ON
+int PUL=12; 
+int DIR=13; 
+
+const int numStepsPerRevolution = 5000; //6400
+//int steps = 60 * numStepsPerRevolution / 360;
+int steps = 4000;
+void setup() {
+  pinMode (PUL, OUTPUT);
+  pinMode (DIR, OUTPUT);
+
+}
+
+void loop() {
+  for (int i=0; i<steps; i++)    //Forward
+  {
+    digitalWrite(DIR,LOW);
+    digitalWrite(PUL,LOW);
+    delayMicroseconds(500);
+    digitalWrite(PUL,HIGH);
+    delayMicroseconds(500);
+  }
+
+
+
+  for (int i=0; i<steps; i++)   //Backward
+  {
+    digitalWrite(DIR,HIGH);
+    digitalWrite(PUL,LOW);
+    delayMicroseconds(500);
+    digitalWrite(PUL,HIGH);
+    delayMicroseconds(500);
+  }
+}
